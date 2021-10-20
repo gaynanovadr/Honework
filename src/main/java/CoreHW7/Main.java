211425f17adea51a85f70fe339770a14a7f9a9cd
gaynanovadr <88196592+gaynanovadr@users.noreply.github.com>
2021-10-20 17:29:53 +0200

    Delete Main.java

--- src/main/java/CoreHW7/Main.java
+++
@@ -1,8 +0,0 @@
-package CoreHW7;
-
-public class Main {
-    public static void main(String[] args) {
-        UserInterfaceView userInterfaceView = new UserInterfaceView();
-        userInterfaceView.runInterface();
-    }
-}
