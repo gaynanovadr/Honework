8898bbf2c598bdcd190e34b021484722a856515d
gaynanovadr <88196592+gaynanovadr@users.noreply.github.com>
2021-10-20 17:27:57 +0200

    Delete WeatherModel.java

--- src/main/java/CoreHW7/WeatherModel.java
+++
@@ -1,9 +0,0 @@
-package CoreHW7;
-
-import java.io.IOException;
-
-public interface WeatherModel {
-    void getWeather(String selectedCity, Variant period) throws IOException;
-
-    void getSavedWeather();
-}
